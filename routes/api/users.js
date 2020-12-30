1bca186751849ffa867625bbd01f9aeb19a15a3d
Isaias Villalobos <isaiasvillalobos98@gmail.com>
2020-12-30 17:20:22 -0500

    Implemented JWT token, aka jot

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,29 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
-const { check, validation, validationResult } = require('express-validator');
-const { Mongoose } = require('mongoose');
-const Users = require('../../models/Users');
-const User = require('../../models/Users');
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validation, validationResult } = require("express-validator");
+const { Mongoose } = require("mongoose");
+const Users = require("../../models/Users");
+const User = require("../../models/Users");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 //route POST /api/users
-//Desc: 
+//Desc:
 //Notes
 //@access: Public route, don't need a token
 router.post(
-  '/',
+  "/",
   [
-    check('name', 'Please include a valid username').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
+    check("name", "Please include a valid username").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
     check(
-      'password',
-      'Please include a password with 10 or more characters'
+      "password",
+      "Please include a password with 10 or more characters"
     ).isLength({ min: 10 }),
   ],
-  //Anon function
+
   async (req, res) => {
     const errors = validationResult(req);
     //check if the there are any errors in the array
@@ -37,14 +37,16 @@ router.post(
 
       // See if user exists in the database
       if (user) {
-        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
-      // console.log(req.body);
+      console.log(req.body);
       const avatar = gravatar.url(email, {
         s: 200,
-        r: 'pg',
-        d: 'mm',
+        r: "pg",
+        d: "mm",
       });
 
       // Create the user
@@ -58,35 +60,36 @@ router.post(
       //Hash the password
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
-      
+
       //Save user to the database.
       await user.save();
-      
+
       //Create the payload
       const payload = {
         users: {
-          id: user.id
-        }
-      }
+          id: user.id,
+        },
+      };
 
       //Sign the token
       jwt.sign(
-        payload, 
+        payload,
         config.get("jwtToken"),
-        {expiresIn: 360000}, 
+        { expiresIn: 360000 },
         (err, token) => {
-          if(err){
+          if (err) {
             throw err;
-          }else{
-            res.json({token});
+          } else {
+            res.json({ token });
           }
         }
       );
+      //If there is an error server responds with message
     } catch (err) {
       // Useful for debugging
       // console.log(err);
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send("Server Error");
     }
   }
 );
