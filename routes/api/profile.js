d9900de800bb909db7d4be2528cb8664c45dda25
Isaias Villalobos <isaias@Isaiass-MacBook-Pro.local>
2021-01-29 20:15:28 -0500

    The profile route now supports removing a user and the profile associated with a user. The route has been tested with Postman.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,4 +157,32 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+
+//route DELETE /api/profile
+//desc: Delete profile, user and posts
+//@access: Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove a user's post
+
+    //Remove Profile 
+    await Profile.findOneAndRemove({ user: req.user.id});
+    //Remove User
+    await User.findOneAndRemove({_id: req.user.id});
+
+
+    const profile = await Profile.findOne({user: req.user.id});
+    const user  = await User.findOne({_id: req.user.id});
+    if (!user && !profile){
+      res.json({ msg: "User removed"});
+    }
+
+    // res.json({ msg: "User removed"});
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
