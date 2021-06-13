e9796481297b659c17290fd15b40f00c3215fb96
Isaias Villalobos <isaiasvillalobos98@gmail.com>
2021-06-13 00:15:52 -0400

    Added routes and endpoints

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { validationResult, check } = require("express-validator");
 const auth = require("../../middleware/auth");
+const { exists } = require("../../models/Post");
 const router = express.Router();
 const Post = require("../../models/Post");
 const Profile = require("../../models/Profile");
@@ -8,7 +9,7 @@ const User = require("../../models/User");
 
 // route POST /api/posts
 // desc: You have have to be logged in to make a post
-// @access: Private route, don't need a token
+// @access: Private route, need a token
 router.post(
   "/",
   [auth, check("text", "Text Required").not().isEmpty()],
@@ -39,5 +40,76 @@ router.post(
     }
   }
 );
+
+
+// route GET /api/posts
+// desc: This will get all posts that have been submitted but a user.
+// @access: Private route, therefore you need a user's token 
+router.get(
+  "/", auth,
+  async (req, res) => {
+    try {
+      const posts = await Post.find().sort({date: 'descending'});
+      res.json(posts);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// route GET /api/posts/:id
+// desc: This will get all posts that have been submitted but a user.
+// @access: Private route, therefore you need a user's token 
+router.get(
+  "/:id", auth,
+  async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id).sort({ date: 'descending' });
+      if (!post){
+        return res.status(404).json({ message: "Post Not Found" });
+      }
+      res.json(post);
+    } catch (err) {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).json({ message: "Post Not Found" });
+      }
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+
+// route DELETE /api/posts/:id
+// desc: Delete a post by a user
+// @access: Private route, therefore you need a user's token 
+router.delete("/:id", auth, async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      
+      // Check if post obj exists
+      if (!post){
+        return res.status(404).json({ message: "Post Not Found" });
+      }
+
+      // Check if post made by user is the one that is passed into parameters
+      if (post.user.toString() !== req.user.id) {
+        return res.status(401).json({ message: "User not authorized" });
+      }
+
+      await post.remove();
+
+
+      res.json(post);
+    } catch (err) {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).json({ message: "Post Not Found" });
+      }
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+
 
 module.exports = router;
